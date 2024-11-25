f6057be8f887a30a15d327a8957e5300e730f22b
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-26 02:04:17 +0400

    define HomeVm for organizing and passing data to the home view

---
+++ LittleFashionFrontToBack/ViewModels/HomeVm.cs
@@ -0,0 +1,10 @@
+﻿using LittleFashionFrontToBack.Model;
+
+namespace LittleFashionFrontToBack.ViewModels;
+
+public class HomeVm
+{
+    public List<Product> Products { get; set; }
+    public List<Category> Categories { get; set; }
+    public List<ProductImage> ProductImages { get; set; }
+}
