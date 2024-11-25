45898ad05353d3b6a1582bab7a7f2dd85f51460c
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-26 02:05:29 +0400

    define Product class inheriting from BaseEntity with properties for product details and relationships

---
+++ LittleFashionFrontToBack/Model/Product.cs
@@ -0,0 +1,15 @@
+﻿using LittleFashionFrontToBack.Model.Base;
+
+namespace LittleFashionFrontToBack.Model;
+
+public class Product : BaseEntity
+{
+    public string Title { get; set; }
+    public string SubTitle { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+    public int Quantity { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public List<ProductImage> ProductImages { get; set; } 
+}
