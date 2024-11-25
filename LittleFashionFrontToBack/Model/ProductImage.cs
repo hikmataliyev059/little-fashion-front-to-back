e8201203427750ecae8133dbc198f8a20dc8af01
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-26 02:05:58 +0400

    define ProductImage class inheriting from BaseEntity with properties for image URL and association with Product

---
+++ LittleFashionFrontToBack/Model/ProductImage.cs
@@ -0,0 +1,11 @@
+﻿using LittleFashionFrontToBack.Model.Base;
+
+namespace LittleFashionFrontToBack.Model;
+
+public class ProductImage : BaseEntity
+{
+    public string ImgUrl { get; set; }
+    public bool Primary { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+}
