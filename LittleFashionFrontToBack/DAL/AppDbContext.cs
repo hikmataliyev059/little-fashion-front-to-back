ee09bc287b61aa02714a572360ccdc3ea9021ff1
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-26 01:58:00 +0400

    configure AppDbContext for data model integration

---
+++ LittleFashionFrontToBack/DAL/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LittleFashionFrontToBack.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LittleFashionFrontToBack.DAL;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Product> Products { get; set; }
+    
+    public DbSet<Category> Categories { get; set; }
+    
+    public DbSet<ProductImage> ProductImages { get; set; }
+    
+    
+}
