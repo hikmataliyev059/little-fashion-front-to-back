810633b467667be6534f68fd812c4628cba24554
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-26 02:03:00 +0400

    implement HomeController to manage product listings and details views

---
+++ LittleFashionFrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using LittleFashionFrontToBack.DAL;
+using LittleFashionFrontToBack.Model;
+using LittleFashionFrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LittleFashionFrontToBack.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly AppDbContext _appDbContext;
+
+    public HomeController(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public IActionResult Index()
+    {
+        List<Product> products = _appDbContext.Products.Include(x => x.ProductImages).ToList();
+
+        HomeVm vm = new HomeVm()
+        {
+            Products = products
+        };
+
+        return View(vm);
+    }
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var product = await _appDbContext.Products
+            .Include(x => x.Category)
+            .Include(x => x.ProductImages)
+            .FirstOrDefaultAsync(x => x.Id == id);
+        
+        return View(product);
+    }
+}
