73ddd192de44c03e7b116f2dc1e4f81001f4588f
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-26 02:09:39 +0400

    set up to configure database context, routing, static files, and middleware for application initialization

---
+++ LittleFashionFrontToBack/Program.cs
@@ -0,0 +1,30 @@
+﻿using LittleFashionFrontToBack.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace LittleFashionFrontToBack;
+
+public class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllersWithViews();
+        
+        builder.Services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DB_URL"));
+        });
+        
+        var app = builder.Build();
+
+        app.MapControllerRoute(
+            name:"default",
+            pattern:"{controller=Home}/{action=Index}/{id?}"
+            );
+
+        app.UseStaticFiles();
+
+        app.Run();
+    }
+}
