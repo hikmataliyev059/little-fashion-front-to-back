eaa782933a9a97f57cd33e81d32444d42153d511
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-11-26 02:01:11 +0400

    create Category class inheriting from BaseEntity with name and products properties

---
+++ LittleFashionFrontToBack/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿using LittleFashionFrontToBack.Model.Base;
+
+namespace LittleFashionFrontToBack.Model;
+
+public class Category : BaseEntity
+{
+    public string Name { get; set; }
+    public List<Product> Products { get; set; }
+}
